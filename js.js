533d11ebfb2f0dcb36212125596209097bd1c358
Valeria <valeriaoshiro19@gmail.com>
2017-01-04 15:14:23 -0800

    Able to add new to-dos

--- js.js
+++ js.js
@@ -1,10 +1,19 @@
 //when li is clicked, it strikethrough and changes color
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
 	$(this).toggleClass('completed');
 });
 
-//when X is clicked, it deletes the todo
-$('span').on('click', function(event){
+//when X is clicked, it deletes the to-do
+$('ul').on('click', 'span', function(event){
 	$(this).parent().remove();
 	event.stopPropagation();
+});
+
+//add new to-dos
+$('input').keypress(function(event){
+	if(event.which === 13){
+		var inputToDo = $('input').val();
+		$('ul').append('<li><span>X</span> ' + inputToDo + '</li>');
+		$('input').val('');
+	}
 });
