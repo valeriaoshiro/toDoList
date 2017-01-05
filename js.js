ac6b0933f30b794fd38a32fc76ee2bfe91fcce6f
Valeria <valeriaoshiro19@gmail.com>
2017-01-05 10:44:19 -0800

    Style project

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ $('ul').on('click', 'li', function(){
 	$(this).toggleClass('completed');
 });
 
-//when X is clicked, it deletes the to-do
+//when trash is clicked, it deletes the to-do
 $('ul').on('click', 'span', function(event){
 	$(this).parent().remove();
 	event.stopPropagation();
@@ -13,7 +13,12 @@ $('ul').on('click', 'span', function(event){
 $('input').keypress(function(event){
 	if(event.which === 13){
 		var inputToDo = $('input').val();
-		$('ul').append('<li><span>X</span> ' + inputToDo + '</li>');
+		$('ul').append('<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ' + inputToDo + '</li>');
 		$('input').val('');
 	}
 });
+
+//when + is clicked, it hides or brings back the input
+$('.fa-plus').on('click', function(){
+	$('input').fadeToggle();
+})
